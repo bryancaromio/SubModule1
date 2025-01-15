48642aecb958e02f2c79679f1253bf9b24562c6b
Bryan Caro <bryancaro@fractalteam.io>
2025-01-15 15:19:16 +0100

    update: implement new func

--- Sources/SubModule1/SubModule1.swift
+++ Sources/SubModule1/SubModule1.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import Foundation
+
+public struct SubModule1 {
+    public init() {}
+    
+    var text = "Hello, World!"
+    
+    public func printText() {
+        print(text)
+    }
+}
